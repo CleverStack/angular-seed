972e0c2fcaccd8a91c9ed777fb683409d23088ae
sdeering <samdeering@gmail.com>
2014-01-17 20:04:46 +1000

    update/unit tests broken

---
+++ app/modules/application/config.js
@@ -0,0 +1,41 @@
+require.config({
+  baseUrl: '/modules/application',
+  packages: [
+    {
+      name: 'cs_account',
+      location: '../cs_account'
+    },
+    {
+      name: 'cs_common',
+      location: '../cs_common'
+    },
+    {
+      name: 'cs_session',
+      location: '../cs_session'
+    },
+    {
+      name: 'users',
+      location: '../users'
+    }
+  ],
+  paths: {
+    angular: '../../components/angular/angular',
+    ngResource: '../../components/angular-resource/angular-resource',
+    ngRoute: '../../components/angular-route/angular-route',
+    ngSanitize: '../../components/angular-sanitize/angular-sanitize',
+  },
+  shim: {
+    angular: {
+      exports: 'angular'
+    },
+    ngResource: {
+      deps: ['angular']
+    },
+    ngRoute: {
+      deps: ['angular']
+    },
+    ngSanitize: {
+      deps: ['angular']
+    }
+  }
+});
---
+++ tests/unit/home.js
@@ -0,0 +1,25 @@
+define(['angular', 'angular-mocks', '../../../scripts/home_controller.js'], function (ng) {
+  'use strict';
+
+  describe('Controller: HomeController', function () {
+
+    // load the controller's module
+    beforeEach(ng.mock.module('app'));
+
+    var HomeCtrl
+      , scope;
+
+    // Initialize the controller and a mock scope
+    beforeEach(ng.mock.inject(function ($controller, $rootScope) {
+      scope = $rootScope.$new();
+      HomeCtrl = $controller('HomeController', {
+        $scope: scope
+      });
+    }));
+
+    it('should attach a list of awesomeThings to the scope', function () {
+      scope.welcome.should.equal('Hello clever!');
+    });
+  });
+
+});
