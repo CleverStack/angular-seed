b793e6bd273e7b4b89def3b072d744ac632ae145
PilsY <richard.gustin86@gmail.com>
2014-11-15 16:49:55 +1000

    chore(cleanup): Module file

--- app/modules/auth/module.js
+++ app/modules/auth/module.js
@@ -6,6 +6,7 @@ define( [ 'angular' ], function( ng ) {
   ng.module( 'auth.services', [] );
   ng.module( 'auth.filters', [] );
   ng.module( 'auth.models', [] );
+  ng.module( 'auth.directives', [] );
 
   var module = ng.module( 'auth', [
     'cs_common',
@@ -24,65 +25,71 @@ define( [ 'angular' ], function( ng ) {
     NavbarProvider.extend({
       right: [
         {
-          label:          'Sign Up',
-          href:           '/signUp',
-          class:          'fa-list-alt',
-          requiresSignIn: false,
-          order:          1,
-          display:        function( $scope ) {
+          label               : 'Sign Up',
+          href                : '/signUp',
+          class               : 'fa-list-alt',
+          requiresSignIn      : false,
+          order               : 1,
+          display             : function( $scope ) {
             return !$scope.currentUser;
           }
         },
         {
-          label: 'Settings',
-          href: '',
-          class: 'fa-cogs',
-          requiresSignIn: true,
-          order: 1,
+          label               : 'Account',
+          href                : '/settings/account',
+          class               : 'fa-institution',
+          requiresSignIn      : true
+        },
+        {
+          label               : 'Settings',
+          href                : '',
+          order               : 1,
+          class               : 'fa-cogs',
+          requiresSignIn      : true,
           subMenu: [
             {
-              label:          'Account',
-              href:           '/settings/account',
-              class:          'fa-institution',
-              requiresSignIn: true,
-              order:          3
+              label           : 'Billing',
+              href            : '/subscription/billing',
+              class           : 'fa-dollar',
+              requiresSignIn  : true,
+              order           : 3
             },
             {
-              label:          'Users',
-              href:           '/settings/users',
-              class:          'fa-user',
-              requiresSignIn: true,
-              order:          4
+              label           : 'Users',
+              href            : '/settings/users',
+              class           : 'fa-user',
+              requiresSignIn  : true,
+              order           : 4
             }
           ]
         },
         {
-          label:          'My Account',
-          click:          function( $scope, $event ) {
+          label               : 'My Account',
+          class               : 'fa-user',
+          order               : 2,
+          click               : function( $scope, $event ) {
             $scope.helpers.openUserModal( $scope.currentUser, $scope.currentUser, true );
             $event.preventDefault();
           },
-          class:          'fa-user',
-          order:          2,
-          display:        function( $scope ) {
+          display             : function( $scope ) {
             return !!$scope.currentUser;
           }
         },
         {
-          label:          'Sign In',
-          href:           '/signIn',
-          class:          'fa-sign-in',
-          order:          2,
-          display:        function( $scope ) {
+          label               : 'Sign In',
+          href                : '/signIn',
+          class               : 'fa-sign-in',
+          order               : 2,
+          display             : function( $scope ) {
             return !$scope.currentUser;
           }
         },
         {
-          label:          'Sign Out',
-          href:           '/signOut',
-          class:          'fa-sign-out',
-          order:          3,
-          display:        function( $scope ) {
+          label               : 'Sign Out',
+          href                : '/signOut',
+          class               : 'fa-sign-out',
+          order               : 3,
+          display             : function( $scope ) {
             return !!$scope.currentUser;
           }
         }
@@ -91,45 +98,44 @@ define( [ 'angular' ], function( ng ) {
 
     $routeProvider
       .when( '/signUp', {
-        templateUrl: TemplateProvider.view( 'auth', 'signUp' ),
-        controller: 'SignUpController',
-        public: true
+        templateUrl : TemplateProvider.view( 'auth', 'signUp' ),
+        controller  : 'SignUpController',
+        public      : true
       })
       .when('/registration_confirm/:accountId/:token', {
-        templateUrl: TemplateProvider.view( 'auth', 'registration_confirm' ),
-        controller: 'RegistrationConfirm',
-        public: true
+        templateUrl : TemplateProvider.view( 'auth', 'registration_confirm' ),
+        controller  : 'RegistrationConfirm',
+        public      : true
       })
       .when( '/signIn', {
-        templateUrl: TemplateProvider.view( 'auth', 'signIn' ),
-        controller: 'SignInController',
-        public: true
+        templateUrl : TemplateProvider.view( 'auth', 'signIn' ),
+        controller  : 'SignInController',
+        public      : true
       })
       .when( '/signOut', {
-        templateUrl: TemplateProvider.view( 'auth', 'partials/signOut' ),
-        controller: 'SignOutController',
-        template: ' ',
-        public: true
+        templateUrl : TemplateProvider.view( 'auth', 'partials/signOut' ),
+        controller  : 'SignOutController',
+        public      : true
       })
-      .when('/password_reset_request', {
-        templateUrl: TemplateProvider.view( 'auth', 'password_reset_request' ),
-        controller: 'PasswordResetRequestController',
-        public: true
+      .when('/requestPasswordReset', {
+        templateUrl : TemplateProvider.view( 'auth', 'requestPasswordReset' ),
+        controller  : 'PasswordResetRequestController',
+        public      : true
       })
-      .when('/password_reset_submit', {
-        templateUrl: TemplateProvider.view( 'auth', 'password_reset_submit' ),
-        controller: 'PasswordResetSubmitController',
-        public: true
+      .when('/resetPassword', {
+        templateUrl : TemplateProvider.view( 'auth', 'resetPassword' ),
+        controller  : 'PasswordResetSubmitController',
+        public      : true
       })
       .when( '/settings/users', {
-        templateUrl: TemplateProvider.view( 'auth', 'users/list' ),
-        controller: 'UsersListController',
-        public: false
+        templateUrl : TemplateProvider.view( 'auth', 'users/list' ),
+        controller  : 'UsersListController',
+        public      : false
       })
-      .when('/account_confirm', {
-        templateUrl: TemplateProvider.view( 'auth', 'account_confirm' ),
-        controller: 'ActivationConfirm',
-        public: true
+      .when('/account/confirm', {
+        templateUrl : TemplateProvider.view( 'auth', 'account/confirm' ),
+        controller  : 'SignUpConfirmController',
+        public      : true
       });
 
   });
