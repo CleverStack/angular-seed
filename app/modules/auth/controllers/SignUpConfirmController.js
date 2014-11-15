b4d549e755348641f256a7b41eff63116f5b32a4
PilsY <richard.gustin86@gmail.com>
2014-11-15 16:49:38 +1000

    feat(account): Confirmation template and controller

---
+++ app/modules/auth/controllers/SignUpConfirmController.js
@@ -0,0 +1,21 @@
+define( [ 'angular', '../module' ], function( ng ) {
+  'use strict';
+
+  ng
+  .module( 'auth.controllers' )
+  .controller( 'SignUpConfirmController', function( $scope, Session, AuthHelpers, $routeParams, $location ) {
+      
+      $scope.$on( 'SessionProvider:signUpConfirmationSuccess', function( event, data ) {
+        Session.authenticate( data.user );
+        $scope.success = true;
+      });
+
+      $scope.$on('SessionProvider:signUpConfirmationFailure', function(){
+        $scope.failure = true;
+      });
+
+      Session.confirmSignUp( { id: $routeParams.u, token: $routeParams.t, name: $routeParams.n } );
+
+    }
+  );
+});
