e5ca8db1c1f9a7792e939ce233a13cf8e07fe2c2
Richard Gustin <richard@rgnconsulting.com.au>
2015-04-18 05:33:00 +1000

    feat(table): Rewrite of CleverTable - WIP

--- app/modules/cs_common/controllers/CleverTableController.js
+++ app/modules/cs_common/controllers/CleverTableController.js
@@ -1,53 +1,53 @@
-define( [ 'angular', 'underscore', 'selectn', '../module' ], function( ng, _, selectn ) {
+define(['angular', 'underscore', 'selectn', '../module'], function(ng, _, selectn) {
   'use strict';
 
   ng
-  .module( 'cs_common.controllers' )
-  .controller( 'CleverTableController', function( $scope, $element, $attrs, $rootScope, $injector, $log, $filter, ngTableParams, Template ) {
-    var NgTableParams = ngTableParams;
+  .module('cs_common.controllers')
+  .controller('CleverTableController', function($scope, $element, $attrs, $rootScope, $injector, $log, $filter, $timeout, ngTableParams, Template) {
+    $scope.service          = $injector.has($attrs.service) ? $injector.get($attrs.service) : false;
 
-    $scope.service      = $injector.has( $attrs.service ) ? $injector.get( $attrs.service ) : false;
-    if ( !$scope.service ) {
+    if (!$scope.service) {
       throw 'You didn\'t provide a service to use with this clever-table';
     }
 
-    $scope.data         = [];
-    $scope.filters      = {};
-    $scope.messenger    = $injector.has( 'Messenger' ) ? $injector.get( 'Messenger' ) : $log;
-    $scope.columnTitles = '';
-    $scope.hasFilters   = false;
-    $scope.rowTemplate      = $scope.rowTemplate !== undefined ? $scope.rowTemplate : Template.view( 'cs_common', 'tableRow' );
+    $scope.data             = [];
+    $scope.filters          = {};
+    $scope.messenger        = $injector.has('Messenger') ? $injector.get('Messenger') : $log;
+    $scope.columnTitles     = '';
+    $scope.hasFilters       = false;
+    $scope.rowTemplate      = $scope.rowTemplate !== undefined ? $scope.rowTemplate : Template.view('cs_common', 'tableRow');
     $scope.showClearFilters = $scope.showClearFilters !== undefined ? $scope.showClearFilters : true;
     $scope.showClearSorting = $scope.showClearSorting !== undefined ? $scope.showClearSorting : true;
 
-    $scope.columns.forEach( function( column ) {
-      $scope.columnTitles += column.title ? column.title : column.name;
-      $scope.columnTitles += ',';
-      $scope.filters[ column.name ] = '';
-      if ( !!column.filter ) {
-        $scope.hasFilters = true;
+    $scope.columns.forEach(function(column) {
+      column.visible        = column.visible !== undefined ? column.visible : true;
+      $scope.columnTitles   += column.title ? column.title : column.name;
+      $scope.columnTitles   += ',';
+      if (!!column.filter) {
+        $scope.hasFilters   = true;
       }
+      $scope.filters[column.name] = '';
     });
-    $scope.columnTitles.substr( 0, $scope.columnTitles.length - 1 );
+    $scope.columnTitles.substr(0, $scope.columnTitles.length - 1);
 
-    $scope.defaultSorting = $scope.defaultSorting || { id: 'asc' };
-    $scope.sorting      = $scope.defaultSorting;
-    $scope.page         = $scope.page || 1;
-    $scope.count        = $scope.count || 10;
+    $scope.defaultSorting   = $scope.defaultSorting || { id: 'asc' };
+    $scope.sorting          = $scope.defaultSorting;
+    $scope.page             = $scope.page || 1;
+    $scope.count            = $scope.count || 10;
 
-    $scope.filterClass = function( column ) {
-      if ( $scope.tableParams.isSortBy( column, 'asc' ) ) {
+    $scope.filterClass = function(column) {
+      if ($scope.tableParams.isSortBy(column, 'asc')) {
         return 'sort-asc';
-      } else if ( $scope.tableParams.isSortBy( column, 'desc' ) ) {
+      } else if ($scope.tableParams.isSortBy(column, 'desc')) {
         return 'sort-desc';
       }
       return '';
     };
 
-    $scope.setSorting = function( column ) {
+    $scope.setSorting = function(column) {
       var sort = {};
       sort[ column ] = $scope.tableParams.isSortBy(column, 'asc') ? 'desc' : 'asc';
-      $scope.tableParams.sorting( sort );
+      $scope.tableParams.sorting(sort);
     };
 
     $scope.resetFilters = function() {
@@ -55,29 +55,35 @@ define( [ 'angular', 'underscore', 'selectn', '../module' ], function( ng, _, se
       $scope.tableParams.filter($scope.filters);
     };
 
-    $scope.outputRow = function( row, columnName ) {
-      return selectn( columnName, row );
+    $scope.outputRow = function(row, columnName) {
+      return selectn(columnName, row);
     };
 
-    $scope.selectFilter = function( column ) {
+    $scope.selectFilter = function(column) {
       var values = [];
-      $scope.data.forEach( function( row ) {
-        values.push( { id: selectn( column.name, row ), title: selectn( column.name, row ) } );
+      $scope.data.forEach(function(row) {
+        values.push({ id: selectn(column.name, row), title: selectn(column.name, row) });
       });
       return values;
     };
 
-    $scope.$on( 'reload', function() {
+    $scope.reload = function() {
       $scope.tableParams.reload();
-    });
+      $timeout(function() {
+        $element.trigger('resize.rc');
+      }, 0);
+    };
 
-    $rootScope.$on( 'table:reload', function() {
-      $scope.tableParams.reload();
-    });
+    $scope.filtersEnabled = false;
+    $scope.toggleFilters = function() {
+      $scope.filtersEnabled = !$scope.filtersEnabled;
+    };
+
+    $scope.$on('reload', $scope.reload);
+    $rootScope.$on('table:reload', $scope.reload);
 
     $scope.filters = {};
 
-    // Directive tableParams
     $scope._params       = {
       page:     $scope.page,
       count:    $scope.count,
@@ -85,50 +91,54 @@ define( [ 'angular', 'underscore', 'selectn', '../module' ], function( ng, _, se
       filter:   $scope.filters
     };
 
-    $scope.tableParams = new NgTableParams(
+    $scope.tableParams = new ngTableParams(
       $scope._params,
       {
-        // debugMode: true,
-        data: $scope.data,
-        getData: function( $defer, params ) {
+        data      : $scope.data,
+        getData   : function($defer, params) {
+
+          $scope.toggleFilter = function(params) {
+            params.settings().$scope.show_filter = !params.settings().$scope.show_filter;
+          };
 
           $scope.service
-          .list( $scope.getDataOptions ? $scope.getDataOptions() : _.extend( { /* _page: params.page(), _pageSize: params.count(), _sorting: params.sorting()*/ } /*, params.filter() */ ) )
-          .then( function( data ) {
-            if ( params.sorting() ) {
-              data = $filter( 'orderBy' )( data, params.orderBy() );
+          .list($scope.getDataOptions ? $scope.getDataOptions() : _.extend({ /* _page: params.page(), _pageSize: params.count(), _sorting: params.sorting()*/ } /*, params.filter() */))
+          .then(function(data) {
+            if (params.sorting()) {
+              data = $filter('orderBy')(data, params.orderBy());
             }
 
-            if ( Object.keys( params.filter() ).length ) {
+            if (Object.keys(params.filter()).length) {
               var _filters = {};
 
-              Object.keys( params.filter() ).forEach( function( filterName ) {
-                if ( params.filter()[ filterName ] !== undefined && params.filter()[ filterName ] !== '' ) {
-                  var fil = selectn( filterName, _filters );
-                  fil = selectn( filterName, params.filter() );
+              Object.keys(params.filter()).forEach(function(filterName) {
+                if (params.filter()[ filterName ] !== undefined && params.filter()[ filterName ] !== '') {
+                  var fil = selectn(filterName, _filters);
+                  fil = selectn(filterName, params.filter());
                 }
               });
 
-              if ( Object.keys( _filters ).length ) {
-                data = _.where( data, _filters );
+              if (Object.keys(_filters).length) {
+                data = _.where(data, _filters);
               }
             }
 
-            params.total( data.length );
+            params.total(data.length);
 
             data = data.slice((params.page() - 1) * params.count(), params.page() * params.count());
             $scope.data = data;
 
-            $defer.resolve( data );
-          }, function( err ) {
+            $defer.resolve(data);
+          }, function(err) {
               throw err;
             })
-          .catch( function( err ) {
-            $defer.reject( err );
-            $scope.messenger.error( ' ' + err.message ? err.message : err );
+          .catch(function(err) {
+            $defer.reject(err);
+            $scope.messenger.error(' ' + err.message ? err.message : err);
           });
-        }
+        },
+        debugMode : true
       }
-    );
+   );
   });
 });
--- app/modules/cs_common/directives/CleverTableDirective.js
+++ app/modules/cs_common/directives/CleverTableDirective.js
@@ -1,14 +1,19 @@
-define( [ 'angular', '../module' ], function( ng ) {
+define(['angular', '../module'], function(ng) {
   'use strict';
 
   ng
-  .module( 'cs_common.directives' )
-  .directive( 'cleverTable', function() {
+  .module('cs_common.directives')
+  .directive('cleverTable', function($timeout) {
     return {
       restrict: 'A',
       scope: true,
       templateUrl: '/modules/cs_common/views/table.html',
-      controller: 'CleverTableController'
+      controller: 'CleverTableController',
+      link: function postLink(elem, attrs, transclude) {
+        $timeout(function () {
+          ng.element(window).trigger('resize');
+        }, 0);
+      }
     };
   });
 
