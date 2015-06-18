040d08d57f485245bdd62c63cbda36c6f16e2575
Richard Gustin <richard@rgnconsulting.com.au>
2015-06-19 04:41:23 +1000

    refactor(services): Should store data internally and return a reference

--- app/modules/auth/services/UserService.js
+++ app/modules/auth/services/UserService.js
@@ -1,28 +1,35 @@
-define( [ 'angular', '../module' ], function( ng ) {
+define(['angular', '../module'], function(ng) {
   'use strict';
 
   ng
-  .module( 'auth.services' )
-  .factory( 'UserService', function( UserModel ) {
-    return {
+  .module('auth.services')
+  .factory('UserService', function(UserModel) {
+    var UserService = {
       model: UserModel,
 
-      list: function( findOptions ) {
-        return UserModel.list( findOptions ).$promise;
+      data: null,
+
+      list: function(findOptions) {
+        return UserModel.list(findOptions).$promise.then(function(users) {
+          UserService.data = users;
+          return UserService.data;
+        });
       },
 
-      get: function( findOptions ) {
-        return UserModel.get( findOptions ).$promise;
+      get: function(findOptions) {
+        return UserModel.get(findOptions).$promise;
       },
 
-      create: function( data ) {
-        return UserModel.create( data ).$promise;
+      create: function(data) {
+        return UserModel.create(data).$promise;
       },
 
-      confirm: function( data ) {
-        return UserModel.confirm( data ).$promise;
+      confirm: function(data) {
+        return UserModel.confirm(data).$promise;
       }
     };
+
+    return UserService;
   });
 
 });
--- app/modules/roles/services/PermissionService.js
+++ app/modules/roles/services/PermissionService.js
@@ -1,24 +1,31 @@
-define( [ 'angular', '../module' ], function( ng ) {
+define(['angular', '../module'], function(ng) {
   'use strict';
 
   ng
-  .module( 'roles.services' )
-  .factory( 'PermissionService', function( PermissionModel ) {
-    return {
+  .module('roles.services')
+  .factory('PermissionService', function(PermissionModel) {
+    var PermissionService = {
+
       model: PermissionModel,
 
-      list: function( findOptions ) {
-        return PermissionModel.list( findOptions ).$promise;
+      data: null,
+
+      list: function(findOptions) {
+        return PermissionModel.list(findOptions).$promise.then(function(permissions) {
+          PermissionService.data = permissions;
+          return PermissionService.data;
+        });
       },
 
-      get: function( findOptions ) {
-        return PermissionModel.get( findOptions ).$promise;
+      get: function(findOptions) {
+        return PermissionModel.get(findOptions).$promise;
       },
 
-      create: function( data ) {
-        return PermissionModel.create( data ).$promise;
+      create: function(data) {
+        return PermissionModel.create(data).$promise;
       }
     };
-  });
 
+    return PermissionService;
+  });
 });
